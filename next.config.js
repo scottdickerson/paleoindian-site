965ca0cf246e653385c0733e8ee53037ef6fdf3c
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2023-03-07 19:04:21 -0600

    chore(build): turn on source maps in production

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    productionBrowserSourceMaps: true,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
         prependData: `@import "variables.scss";`,
