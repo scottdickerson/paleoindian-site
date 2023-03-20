eb8b53f4aeb71394d1a02288b3ffd641aa7ffc2f
DanMyers300 <contact+github@danmyers.net>
2023-03-20 14:17:29 -0500

    need to change image on click

--- src/components/flipper.tsx
+++ src/components/flipper.tsx
@@ -6,7 +6,7 @@ export const Flipper = () => {
     const flipperImages = FlipperImages.map((flipperImage) => flipperImage.closed)
 
     const handleFlipperImageClick = () => {
-        console.log('flipper image clicked')
+        console.log('Flipper image clicked')
     }
     
     return (
