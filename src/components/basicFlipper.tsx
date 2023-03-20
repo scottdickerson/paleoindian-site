4dbd84432ad083c9fdc49bbb310a746145931723
DanMyers300 <contact+github@danmyers.net>
2023-03-20 18:13:13 -0500

    still showing 12 images

--- src/components/basicFlipper.tsx
+++ src/components/basicFlipper.tsx
@@ -1,15 +1,13 @@
-import styles from '../styles/Flipper.module.css'
+import styles from '@/styles/Flipper.module.scss'
 import Image from 'next/image'
 import { FlipperImages } from '@/data/ice-age-texas/flipper/flipperData'
 
 export default function DansFlipper() {
-    
     const flipperImages = FlipperImages;
-
     return (
         <div className={styles.flipperWrapper}>
             {flipperImages.map((image) => (
-                <div>
+                <div key={image.id}>
                     <Image 
                         src={image.closed}
                         className={`${styles.flipperImage} ${styles[image.id]}`}
