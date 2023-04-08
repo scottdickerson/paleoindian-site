d5f771d195ba4b07d882baac0889bcc6f38b5a4d
Scott Dickerson <swiftfoot@hotmail.com>
2023-04-08 10:38:05 -0500

    flipper(responsive): fix text to be more responsive

--- src/components/flipper.tsx
+++ src/components/flipper.tsx
@@ -1,40 +1,53 @@
 import styles from '@/styles/Flipper.module.scss'
-import Image from 'next/image'
-import { useState } from 'react';
+import Image, { StaticImageData } from 'next/image'
+import { useState } from 'react'
 
 type Images = {
-  opened: Images;
-  closed: Images;
-  openedText: Text;
-  closedText: Text;
+    opened: StaticImageData
+    closed: StaticImageData
+    openedText: string
+    closedText: string
 }
 
 type Props = {
-  images: Images[];
+    images: Images[]
 }
 
 export default function Flipper({ images }: Props) {
-  const [isOpened, setIsOpened] = useState(new Array(images.length).fill(false));
+    const [isOpened, setIsOpened] = useState(
+        new Array(images.length).fill(false)
+    )
 
-  const toggleIsOpened = (index:any) => {
-    const newIsOpened = [...isOpened];
-    newIsOpened[index] = !newIsOpened[index];
-    setIsOpened(newIsOpened);
-  };
-  return (
-    <div className={styles.flipperWrapper}>
-      {images.map((image:any, index:any) => (
-        <div key={index} className={styles.imageWrapper} onClick={() => toggleIsOpened(index)}>
-          <Image
-            src={isOpened[index] ? image.opened : image.closed}
-            className={styles.flipper}
-            alt={`Flipper ${index + 1}`}
-          />
-          <div className={`${styles.text} ${isOpened[index] ? styles.textIsOpened : styles.textIsClosed}`}>
-            {isOpened[index] ? image.openedText : image.closedText}
-          </div>
+    const toggleIsOpened = (index: any) => {
+        const newIsOpened = [...isOpened]
+        newIsOpened[index] = !newIsOpened[index]
+        setIsOpened(newIsOpened)
+    }
+    return (
+        <div className={styles.flipperWrapper}>
+            {images.map((image: any, index: any) => (
+                <div
+                    key={index}
+                    className={styles.imageWrapper}
+                    onClick={() => toggleIsOpened(index)}
+                >
+                    <Image
+                        src={isOpened[index] ? image.opened : image.closed}
+                        className={styles.flipper}
+                        alt={`Flipper ${index + 1}`}
+                        fill
+                    />
+                    <div
+                        className={`${styles.text} ${
+                            isOpened[index]
+                                ? styles.textIsOpened
+                                : styles.textIsClosed
+                        }`}
+                    >
+                        {isOpened[index] ? image.openedText : image.closedText}
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    )
 }
