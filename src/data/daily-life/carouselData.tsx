a67e4e15b89624349c743ecf62be8f769f30cb33
DanMyers300 <contact+github@danmyers.net>
2023-04-15 16:17:38 -0500

    added some images

--- src/data/daily-life/carouselData.tsx
+++ src/data/daily-life/carouselData.tsx
@@ -1,3 +1,28 @@
-export const carouselData = [
+import carousel1 from '@/data/daily-life/1.jpg'
+import carousel2 from '@/data/daily-life/2.jpg'
+import carousel3 from '@/data/daily-life/3.jpg'
+import carousel4 from '@/data/daily-life/4.jpg'
+import carousel5 from '@/data/daily-life/5.jpg'
 
+export const carouselData = [
+    {
+        id: 'carouselone',
+        image: carousel1,
+    },
+    {
+        id: 'carouseltwo',
+        image: carousel2,
+    },
+    {
+        id: 'carouselthree',
+        image: carousel3,
+    },
+    {
+        id: 'carouselfour',
+        image: carousel4,
+    },
+    {
+        id: 'carouselfive',
+        image: carousel5,
+    },
 ]
