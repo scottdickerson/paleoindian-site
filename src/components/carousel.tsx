c5c1ed624169c7870e582eb8a52015afecf3ab19
DanMyers300 <contact+github@danmyers.net>
2023-04-17 17:09:41 -0500

    drag works

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import { useRef } from 'react';
 import Image, { StaticImageData } from 'next/image';
 import styles from '../styles/Carousel.module.scss';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { useDraggable } from "react-use-draggable-scroll";
 
 type Images = {
     id: string;
@@ -12,8 +15,15 @@ type Props = {
 }
 
 export function DailyLifeCarousel({ images }: Props) {
+    // We will use React useRef hook to reference the wrapping div:
+    const ref = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+    const { events } = useDraggable(ref); // Now we pass the reference to the useDraggable hook:
+
     return (
-        <div className={styles.carouselWrapper} >
+        <div className={styles.carouselWrapper} 
+        {...events}
+        ref={ref} // add reference and events to the wrapping div
+        >
             {images.map((image: any, index: any) => (
                 <div className={styles.imageWrapper} key={index}>
                     <Image
