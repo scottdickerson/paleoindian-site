4dcf5ab1c2742e70d28ef536567d574b912a3fde
DanMyers300 <contact+github@danmyers.net>
2023-04-17 14:08:09 -0500

    found out carousels are considered bad UX going to try css scroll snap

--- src/data/daily-life/carouselData.tsx
+++ src/data/daily-life/carouselData.tsx
@@ -2,7 +2,7 @@ import carousel1 from '@/data/daily-life/1.jpg'
 import carousel2 from '@/data/daily-life/2.jpg'
 import carousel3 from '@/data/daily-life/3.jpg'
 import carousel4 from '@/data/daily-life/4.jpg'
-import carousel5 from '@/data/daily-life/5.jpg'
+// import carousel5 from '@/data/daily-life/5.jpg'
 
 export const carouselData = [
     {
@@ -21,8 +21,8 @@ export const carouselData = [
         id: 'carouselfour',
         image: carousel4,
     },
-    {
-        id: 'carouselfive',
-        image: carousel5,
-    },
+    // {
+    //     id: 'carouselfive',
+    //     image: carousel5,
+    // },
 ]
