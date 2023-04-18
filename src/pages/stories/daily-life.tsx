25ad24b7474cfe78048838a947cb449d305a0a54
Colloke <github.p5x18@simplelogin.co>
2023-04-18 16:51:57 -0500

    finished pasting in text for main page

--- src/pages/stories/daily-life.tsx
+++ src/pages/stories/daily-life.tsx
@@ -54,7 +54,7 @@ const DAILY_LIFE_TEXAS_SECTIONS: StorySectionProps[] = [
         details: (
             <>
             <p>
-            Until recently, illustrations of Paleoindian life almost always featured guys with stone spears…and not much else. We need to remember that the earliest peoples made things that are far rarer than stone in the archeological record. Woven mats and sandals, bone tools, and ceremonial items have all been found in Texas sites, usually in caves where cool, dry air helps preserve these precious clues to the lives of Paleoindians. 
+                Until recently, illustrations of Paleoindian life almost always featured guys with stone spears…and not much else. We need to remember that the earliest peoples made things that are far rarer than stone in the archeological record. Woven mats and sandals, bone tools, and ceremonial items have all been found in Texas sites, usually in caves where cool, dry air helps preserve these precious clues to the lives of Paleoindians. 
             </p>
             </>
         ),
@@ -62,19 +62,20 @@ const DAILY_LIFE_TEXAS_SECTIONS: StorySectionProps[] = [
     {
         title: 'What was for dinner?',
         id: 'what-was-for-dinner',
-        summary: "",
+        summary: "Maybe something easy to get?",
         description:
-            "<p></p>",
+            `<p>Paleoindians did hunt mammoths and other mega-fauna, but not every day. Would you like to bust your hump hunting a dangerous animal or would you just kick back and catch a fish?
+            Check out what was on the menu. </p>`,
         interactive: (
             <Cards cards={cardData} pdf="/pdfs/Ice Age Texas Cards.pdf" />
         ),
         details: (
             <>
             <p>
-
+                It’s fun to think about the skills and talent needed to take home a mammoth for dinner, but archeologists now know that people ate a lot of other things that were way easier to manage. How do we know what Paleoindians ate for dinner? Many different fields of study can help answer that question. Zooarcheologists study animal bones found in archeological sites. Paleoethnobotanists study charred seeds from campfires. Lithic analysts study tiny residue of animal blood and plant starches left on tools.
             </p>
             <p>
-
+                We know that Paleoindians hunted and trapped lots of different animals and ate plenty of plants. Although hunters are often depicted as men, women and children did their fair share of work to keep everybody fed. Animals like deer, rabbits, turtles, and fish would have been a reliable source of protein. Native Texas plants, like pecans, tubers, and wild onions, provided nutrition and flavor. 
             </p>
             </>
         ),
@@ -82,9 +83,10 @@ const DAILY_LIFE_TEXAS_SECTIONS: StorySectionProps[] = [
     {
         title: 'Life Lessons',
         id: 'life-lessons',
-        summary: "",
+        summary: "Ancestors of Indigenous people were the first to learn the landscape.",
         description:
-            "<p></p>",
+            `<p>Their knowledge was important for surviving and thriving, and it has been passed down through generations. Today, those traditions are still around, often with a modern twist. 
+            Hear from Native American people below about their favorite traditions. </p>`,
         interactive: (
             <>
             <p>NEED TO CHANGE VIDEOS</p>
@@ -94,7 +96,7 @@ const DAILY_LIFE_TEXAS_SECTIONS: StorySectionProps[] = [
         details: (
             <>
             <p>
-
+                Native American ancestors became experts in selecting native plants for food and medicine. They knew the best season and location for harvesting specific plants or hunting their favorite game. They learned where to find the right kind of stone for tool making. And they learned to navigate from place to place, finding the best places to cross a river or a mountain. They passed this knowledge and deep relationship with the landscape down through the generations, where it still flows today. 
             </p>
             </>
         ),
@@ -102,16 +104,17 @@ const DAILY_LIFE_TEXAS_SECTIONS: StorySectionProps[] = [
     {
         title: 'Paleo Pets',
         id: 'palaeo-pets',
-        summary: "",
+        summary: "Our furry friends have a long history",
         description:
-            "<p></p>",
+            `<p>Over 20,000 years ago, wolves and humans figured out that they could help each other survive. They joined forces, and the world’s oldest friendship began. 
+            Sit, roll over and test your doggy knowledge below!</p>`,
         interactive: (
             <PetQuiz />
         ),
         details: (
             <>
             <p>
-
+                When Paleoindians first came to the Americas, domesticated dogs were trotting along beside them. Bones and other artifacts discovered around ancient camp sites tell us a lot about man’s best friend. By studying DNA, we know that dogs and people were together long before they came to North America. In fact, Native Americans’ dogs were descended from a line of grey wolves in Eurasia. Scientists can trace their DNA back through generations to learn more about how dogs – and their friendships with people – changed over time. 
             </p>
             </>
         ),
@@ -119,9 +122,10 @@ const DAILY_LIFE_TEXAS_SECTIONS: StorySectionProps[] = [
     {
         title: 'Written in Stone',
         id: 'written-in-stone',
-        summary: '.',
+        summary: 'Stone tools tell a story – if you ask the right questions',
         description:
-            "<p></p>",
+            `<p>When archeologists find stone tools, even broken pieces, they open a window into the past. They carefully document the artifact’s position and context and then study it in the lab. 
+            Scroll through these Paleoindian artifacts. </p>`,
         interactive: (
             <DailyLifeCarousel
                 images={carouselData}
@@ -130,7 +134,7 @@ const DAILY_LIFE_TEXAS_SECTIONS: StorySectionProps[] = [
         details: (
             <>
                 <p>
-
+                    When collectors take stone tools, all this potential information is lost. Additionally, they may disturb a grave or other sacred spot. It’s very important to never dig or disturb a place where ancient Indigenous people lived.
                 </p>
             </>
         ),
