42e01fda6a8ab7286503bf2d9ec4266fd62b9e2a
Colloke <github.p5x18@simplelogin.co>
2023-04-23 16:19:03 -0500

    changed up css completely

--- src/pages/educator-guide.tsx
+++ src/pages/educator-guide.tsx
@@ -11,45 +11,43 @@ const EducatorGuide = () => {
                 <title>Texas Paleoindian - Educator Guide</title>
             </Head>
             <Header />
-            <div
-                className={styles.contentWrapper}
-            >
-                <h1 
-                    className={styles.title}>Educator Guide
-                </h1>
-                <div className={styles.textWrapper}>
+            <div className={styles.contentWrapper}>
+                <div className={styles.leftColumn}>
+                    <h1 className={styles.title}>Educator Guide</h1>
+                    <div className={styles.textWrapper}>
                     <p className={styles.firstParagraph}>
-                        The Texas Paleoindian Project is a collaborative effort between the Texas Archeological Research Laboratory (TARL) at The University of Texas at Austin and the Texas Historical Commission (THC). The project is funded by the National Science Foundation (NSF) and the Texas Parks and Wildlife Department (TPWD). The project is a multi-year effort to document the Paleoindian archaeological record in Texas. The project is led by Dr. David Meltzer, Dr. David Anderson, and Dr. John K. Brugge.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo integer malesuada nunc vel risus commodo.
                     </p>
                     <p className={styles.secondParagraph}>
-                        The Texas Paleoindian Project is a collaborative effort between the Texas Archeological Research Laboratory (TARL) at The University of Texas at Austin and the Texas Historical Commission (THC). The project is funded by the National Science Foundation (NSF) and the Texas Parks and Wildlife Department (TPWD). The project is a multi-year effort to document the Paleoindian archaeological record in Texas. The project is led by Dr. David Meltzer, Dr. David Anderson, and Dr. John K. Brugge.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo integer malesuada nunc vel risus commodo. Purus ut faucibus pulvinar elementum integer enim. 
                     </p>
-                </div>
-                <div className={styles.imageWrapper}>
-                    <div className={styles.image}>Image goes here</div>
-                    <p className={styles.caption}>
-                        Caption goes here
-                    </p>
-                </div>
-                <div className={styles.buttonWrapper}>
-                <Link 
-                    href="/" 
-                    className={`
+                    </div>
+                    <div className={styles.buttonWrapper}>
+                    <Link 
+                        href="/" 
+                        className={`
                         ${styles.button} 
                         ${styles.guideButton}
-                    `}>
-                    Download Guide
-                </Link>
-                <Link 
-                    href="/" 
-                    className={`
+                        `}
+                    >
+                        Download Guide
+                    </Link>
+                    <Link 
+                        href="/" 
+                        className={`
                         ${styles.button} 
                         ${styles.gameButton}
-                    `}>
-                    Download Game
-                </Link>
+                        `}
+                    >
+                        Download Game
+                    </Link>
+                    </div>
                 </div>
-            </div>
+                <div className={styles.rightColumn}>
+                    <div className={styles.image}>Image goes here</div>
+                    <p className={styles.caption}>Caption goes here</p>
+                </div>
+                </div>
             <Footer />
         </>
     )
