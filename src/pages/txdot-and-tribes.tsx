31889e1feae5903c08026167d058a2879c0f5235
Colloke <github.p5x18@simplelogin.co>
2023-04-24 17:30:22 -0500

    main grid layout done

--- src/pages/txdot-and-tribes.tsx
+++ src/pages/txdot-and-tribes.tsx
@@ -17,10 +17,10 @@ const TXDotAndTribes = () => {
             </Head>
             <Header />
             <div className={styles.contentWrapper}>
-                    <p>
+                    <p className={`${styles.textBlock} ${styles.openingParagraph}`}>
                         From time immemorial, the land we think of as Texas was the homeland of diverse groups of Native Peoples, including the ancestors of the many named Tribes that are still active communities today. Through conquest, deceit, and theft, this land was colonized by Europeans and ultimately became the State of Texas. 
                     </p>
-                    <div className={styles.imageBlock1}>
+                    <div className={`${styles.imageBlock} ${styles.imageBlock1}`}>
                         <Image 
                             src={groupImage}
                             alt='group'
@@ -32,7 +32,7 @@ const TXDotAndTribes = () => {
                             Courtesy: Vanesa Brashier, The Courier of Montgomery County
                         </p>
                     </div>
-                    <div className={styles.imageBlock2}>
+                    <div className={`${styles.imageBlock} ${styles.imageBlock2}`}>
                         <Image
                             src={groupImageTwo}
                             alt='group'
@@ -44,7 +44,7 @@ const TXDotAndTribes = () => {
                             Courtesy of Texas Department of Transportation
                         </p>
                     </div>
-                    <div className={styles.textBlock1}>
+                    <div className={`${styles.textBlock} ${styles.textBlock1}`}>
                         <p>
                             Today the descendants of Texas’ Native Peoples live modern lives and are a part of the fabric of our shared community. Tribal leaders, teachers, and individual people still live and pass on their unique cultures. This wisdom is an important way for all Texans to learn about the heritage of Texas’ Native Peoples.
                         </p>
@@ -55,7 +55,7 @@ const TXDotAndTribes = () => {
                             A lot of archeologists work in universities or at museums, but there are also archeologists working at the Texas Department of Transportation.
                         </p>
                     </div>
-                    <div className={styles.imageBlock3}>
+                    <div className={`${styles.imageBlock} ${styles.imageBlock3}`}>
                         <Image
                             src={archeologists}
                             alt='archeologists'
@@ -67,7 +67,7 @@ const TXDotAndTribes = () => {
                             Courtesy of Texas Department of Transportation
                         </p>
                     </div>
-                    <div className={styles.textBlock2}>
+                    <div className={`${styles.textBlock} ${styles.textBlock2}`}>
                         <p>
                             The Texas Department of Transportation is an official department within the State of Texas. TxDOT creates roads, bridges, and other important transportation infrastructure. Sometimes, before construction, TxDOT archeologists find  things made a long time ago by the ancestors of modern-day Tribal peoples. When that happens, TxDOT archeologists carefully excavate these items before any construction goes forward.
                         </p>
@@ -75,7 +75,7 @@ const TXDotAndTribes = () => {
                             TxDOT also consults with many named, diverse Tribes whose ancestors have a relationship to Texas. They work together to ensure that archeological sites and artifacts are studied, respected, and protected.  This collaboration adds to our understanding of the rich heritage of Texas’ Native Peoples.
                         </p>
                     </div>
-                    <div className={styles.imageBlock4}>
+                    <div className={`${styles.imageBlock} ${styles.imageBlock4}`}>
                         <Image 
                             src={presentation}
                             alt='presentation'
@@ -87,7 +87,7 @@ const TXDotAndTribes = () => {
                             Courtesy of Texas Department of Transportation
                         </p>
                     </div>
-                    <div className={styles.textBlock3}>
+                    <div className={`${styles.textBlock} ${styles.textBlock3}`}>
                         <p>
                             TxDOT and Tribal representatives work together to share what they’ve learned with people from all across the state. They do this through workshops, publications, teacher conferences, and special projects. This exhibit, A Time Before Texas, is one such project.
                         </p>
@@ -101,7 +101,7 @@ const TXDotAndTribes = () => {
                     <p>
                     We hope you enjoy learning about A Time Before Texas.
                     </p>
-                    <div className={styles.endingImageBlock}>
+                    <div className={`${styles.imageBlock} ${styles.endingImageBlock}`}>
                         <Image 
                             src={endingImage}
                             alt='endingImage'
