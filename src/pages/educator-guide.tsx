a98df1d8b4898f2562d581d3e01871711e3be56b
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2023-04-26 17:56:41 -0500

    font(responsive): non responsive fonts

--- src/pages/educator-guide.tsx
+++ src/pages/educator-guide.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '@/styles/EducatorGuide.module.scss'
+import sectionStyles from '@/styles/StorySection.module.scss'
 import Link from 'next/link'
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
@@ -14,49 +15,65 @@ const EducatorGuide = () => {
             </Head>
             <Header />
             <div className={styles.contentWrapper}>
-                <div className={styles.textAndButtonColumn}>
+                <div className={styles.textßAndButtonColumn}>
                     <h1 className={styles.title}>Educator Guide</h1>
-                    <p className={`${styles.text} ${styles.firstParagraph}`}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo integer malesuada nunc vel risus commodo.
+                    <p
+                        className={`${sectionStyles.description} ${styles.firstParagraph}`}
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Leo integer malesuada nunc vel risus
+                        commodo.
                     </p>
-                    </div>
-                <div className={styles.imageColumn}>
+                </div>
+                <div className={styles.secondColumn}>
                     <div className={styles.imageWrapper}>
-                    <Image 
-                        src={tempPhoto}
-                        alt="temp photo"
-                        className={styles.image}
-                    />
-                    <p className={styles.caption}>Caption level content will describe the photo in this space.</p>
+                        <Image
+                            src={tempPhoto}
+                            alt="temp photo"
+                            className={styles.image}
+                        />
+                        <p className={styles.caption}>
+                            Caption level content will describe the photo in
+                            this space.
+                        </p>
                     </div>
                 </div>
-                <div className={styles.secondTextandButtonColumn}>
-                    <p className={`${styles.text} ${styles.secondParagraph}`}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo integer malesuada nunc vel risus commodo. Purus ut faucibus pulvinar elementum integer enim. 
+                <div
+                    className={`${styles.thirdColumn} ${sectionStyles.details}`}
+                >
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Leo integer malesuada nunc vel risus
+                        commodo. Purus ut faucibus pulvinar elementum integer
+                        enim.
                     </p>
                     <div className={styles.buttonWrapper}>
-                    <Link 
-                        href="/" 
-                        className={`
+                        <Link
+                            href="/"
+                            className={`
+                            ${sectionStyles.title}
                         ${styles.button} 
                         ${styles.guideButton}
                         `}
-                    >
-                        Download Guide
-                    </Link>
-                    <Link 
-                        href="/" 
-                        className={`
+                        >
+                            Download Guide
+                        </Link>
+                        <Link
+                            href="/"
+                            className={`
+                            ${sectionStyles.title}
                         ${styles.button} 
                         ${styles.gameButton}
                         `}
-                    >
-                        Download Game
-                    </Link>
+                        >
+                            Download Game
+                        </Link>
                     </div>
                 </div>
             </div>
-            <Footer className={styles.footer}/>
+            <Footer className={styles.footer} />
         </>
     )
 }
