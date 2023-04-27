af68b1b988848fa1aed45a6a9ffa641e4446998a
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2023-04-27 17:51:02 -0500

    educator-guide: flip the content based on size

--- src/pages/educator-guide.tsx
+++ src/pages/educator-guide.tsx
@@ -2,78 +2,77 @@ import Head from 'next/head'
 import styles from '@/styles/EducatorGuide.module.scss'
 import sectionStyles from '@/styles/StorySection.module.scss'
 import Link from 'next/link'
-import { Header } from '@/components/header'
-import { Footer } from '@/components/footer'
+import classNames from 'classnames'
 import Image from 'next/image'
 import tempPhoto from '@/data/educator-guide/temp_photo_box.png'
+import { useMediaQuery } from 'react-responsive'
+
+const DetailsSection = () => (
+    <div className={classNames(sectionStyles.details)}>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo
+            integer malesuada nunc vel risus commodo. Purus ut faucibus pulvinar
+            elementum integer enim.
+        </p>
+        <div className={styles.buttonWrapper}>
+            <Link
+                href="/pdfs/Ice Age Texas Cards.pdf"
+                download
+                className={sectionStyles.title}
+            >
+                Download Guide
+            </Link>
+            <Link
+                href="/pdfs/Ice Age Texas Cards.pdf"
+                download
+                className={sectionStyles.title}
+            >
+                Download Game
+            </Link>
+        </div>
+    </div>
+)
 
 const EducatorGuide = () => {
+    const isTwoPane = useMediaQuery({ query: '(min-width: 768px)' })
     return (
         <>
             <Head>
                 <title>Texas Paleoindian - Educator Guide</title>
             </Head>
-            <Header />
+
             <div className={styles.contentWrapper}>
-                <div className={styles.textßAndButtonColumn}>
-                    <h1 className={styles.title}>Educator Guide</h1>
-                    <p
-                        className={`${sectionStyles.description} ${styles.firstParagraph}`}
+                <div>
+                    <h1
+                        className={classNames(
+                            styles.title,
+                            sectionStyles.summary
+                        )}
                     >
+                        Educator Guide
+                    </h1>
+                    <p className={classNames(sectionStyles.description)}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore
                         magna aliqua. Leo integer malesuada nunc vel risus
                         commodo.
                     </p>
+                    {isTwoPane && <DetailsSection />}
                 </div>
-                <div className={styles.secondColumn}>
-                    <div className={styles.imageWrapper}>
-                        <Image
-                            src={tempPhoto}
-                            alt="temp photo"
-                            className={styles.image}
-                        />
-                        <p className={styles.caption}>
-                            Caption level content will describe the photo in
-                            this space.
-                        </p>
-                    </div>
-                </div>
-                <div
-                    className={`${styles.thirdColumn} ${sectionStyles.details}`}
-                >
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Leo integer malesuada nunc vel risus
-                        commodo. Purus ut faucibus pulvinar elementum integer
-                        enim.
+                <div className={styles.imageWrapper}>
+                    <Image
+                        src={tempPhoto}
+                        alt="temp photo"
+                        className={styles.image}
+                    />
+                    <p className={styles.caption}>
+                        Caption level content will describe the photo in this
+                        space.
                     </p>
-                    <div className={styles.buttonWrapper}>
-                        <Link
-                            href="/"
-                            className={`
-                            ${sectionStyles.title}
-                        ${styles.button} 
-                        ${styles.guideButton}
-                        `}
-                        >
-                            Download Guide
-                        </Link>
-                        <Link
-                            href="/"
-                            className={`
-                            ${sectionStyles.title}
-                        ${styles.button} 
-                        ${styles.gameButton}
-                        `}
-                        >
-                            Download Game
-                        </Link>
-                    </div>
                 </div>
+                {!isTwoPane && <DetailsSection />}
             </div>
-            <Footer className={styles.footer} />
         </>
     )
 }
