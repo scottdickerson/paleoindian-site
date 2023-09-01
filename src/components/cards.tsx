ad032c00cadde01fd3d84d2789d83b10a7024374
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2023-09-01 18:32:22 -0500

    fix(contrast): handle color contrast issues for AA

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -113,7 +113,7 @@ const Cards = ({ cards, pdf, cardBack }: CardProps) => {
                         )
                     })}
                 </div>
-                <div id="text" className={styles.background}></div>
+                <div className={styles.background}></div>
                 <span className={styles.text}>{cards[current].caption}</span>
                 <a className={styles.downloadButton} href={pdf} download>
                     <span className={styles.playAlong}>Play along!</span> Click
